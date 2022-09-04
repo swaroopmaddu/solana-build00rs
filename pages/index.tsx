a69a2f220a155e571ef555a85e811766440dd1b0
Swaroop Maddu <swaroop@strawhat.xyz>
2022-09-04 15:59:15 +0530

    Update link twitter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {Box,Center, Spacer, Stack} from '@chakra-ui/react'
+import {Box,Center, Spacer, Stack, Text, Link } from '@chakra-ui/react'
 import NavBar from '../components/NavBar';
 import Disconnected from '../components/Disconnected'
 import Connected from '../components/Connected';
@@ -21,29 +21,24 @@ const Home: NextPage = () => {
       <Box
         w="full"
         h="calc(100vh)"
-        bgImage={connected ? "" :"url(/background.svg)"}
+        bgImage={connected ? "" : "url(/background.svg)"}
         backgroundPosition="center"
       >
         <Stack w="full" h="calc(100vh)" justify="center">
           <NavBar />
           <Spacer />
 
-          <Center>
-            {
-              connected ? <Connected/> : <Disconnected/>
-            }
-          </Center>
+          <Center>{connected ? <Connected /> : <Disconnected />}</Center>
           <Spacer />
 
           <Center>
             <Box marginBottom={4} color="white">
-              <a
-                href="https://twitter.com/madduswaroop"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Follow me on Twitter
-              </a>
+              <Text>
+                Follow me on Twitter&nbsp;
+                <Link color='blue.500' href="https://twitter.com/madduswaroop" isExternal>
+                  @madduswaroop
+                </Link>
+              </Text>
             </Box>
           </Center>
         </Stack>
